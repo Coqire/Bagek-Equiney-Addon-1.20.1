0a1961b411b019e88beec6b7a66f0e72c693e770
Coqire <megen.horne@hotmail.com>
2025-08-01 23:47:27 +1000

    - Fixing tab

--- src/main/java/com/coqire/bageksequineyaddon/item/BageksTackCreativeModTab.java
+++ src/main/java/com/coqire/bageksequineyaddon/item/BageksTackCreativeModTab.java
@@ -11,11 +11,11 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
-public class BageksAddonCreativeModTab {
+public class BageksTackCreativeModTab {
     public static final DeferredRegister<CreativeModeTab> REGISTRY;
     public static final RegistryObject<CreativeModeTab> MAIN;
 
-    public BageksAddonCreativeModTab() {
+    public BageksTackCreativeModTab() {
     }
 
     public static void init(IEventBus bus) {REGISTRY.register(bus);
@@ -23,21 +23,20 @@ public class BageksAddonCreativeModTab {
 
     static {
         REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, "bageksaddon");
-        MAIN = REGISTRY.register("bageks_tab", () -> CreativeModeTab.builder().title(Component.translatable("creativetab.bageks_tab")).icon(() -> new ItemStack(ModItems.BAGUETTE.get()))
+        MAIN = REGISTRY.register("btack_tab", () -> CreativeModeTab.builder()
+                .title(Component.translatable("creativetab.btack_tab")).icon(() -> new ItemStack(ModItems.BAGUETTE.get()))
                 .displayItems((pParameters, pOutput) ->
 
                         BageksTack.REGISTRY.getEntries().forEach((registeredItem) -> {
                     Item item = registeredItem.get();
                     pOutput.accept(item);
 
+
                     pOutput.accept(ModItems.BAGUETTE.get());
                     pOutput.accept(ModItems.BLUE_DONUT_TREAT.get());
                     pOutput.accept(ModItems.PINK_DONUT_TREAT.get());
                     pOutput.accept(ModItems.YELLOW_DONUT_TREAT.get());
-                            pOutput.accept(ModBlocks.BAGEK_WORKBENCH.get());
 
-                    
-                    
 
                 })).withSearchBar().build());
     }
